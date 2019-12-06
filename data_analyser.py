00570c79da8e50de87facb13ec3274faed477f2d
rafle <rafael.leal@runbox.com>
2019-12-06 14:50:28 +0200

    More clustering methods

--- data_analyser.py
+++ data_analyser.py
@@ -7,33 +7,40 @@ import seaborn as sns
 import scipy as sp
 
 import scipy.cluster.hierarchy as sch
-from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import AgglomerativeClustering, KMeans
+from sklearn.decomposition import PCA
 
 
-SPLITTABLE_COLS = ('hd_pos', 'hd_deprel', 'hd_case', 'dp_pos')
+#  EXPANDABLE_COLS = ('hd_pos', 'hd_deprel', 'hd_case', 'dp_pos')
 
 class DataAnalyser:
-    def __init__(self, dataframe_collection):
-        self.dataframe = dataframe_collection
-        self.data = self.handle_data(self.dataframe, SPLITTABLE_COLS)
+    def __init__(self, dataframe_collection, expandable_cols):
+        self.df_collection = dataframe_collection
+        self.data = self.simplify_df():  #  self.df_collection, EXPANDABLE_COLS)
+        self.expanded_columns = self.mark_expandable_cols(expandable_cols)
 
 
-    def mark_splittable_columns(self, dataframe, splittable_cols, limit=3):
-        concat_df = pd.concat(dataframe.values())
+    def get_data(self):
+        return self.data
+
+
+    def mark_expandable_cols(self, expandable_cols, limit=3):
+        expandable_cols = expandable_cols or self.expanded_columns
+        concat_df = pd.concat(self.df_collection.values())
         columns = OrderedDict()
         for col_name in concat_df.keys():
-            if col_name in splittable_cols:
+            if col_name in expandable_cols:
                 columns[col_name] = concat_df[col_name].value_counts()[:limit].keys()
             else:
                 columns[col_name] = []
         return columns
 
 
-    def create_vector(self, lang, dataframe, splittable):
+    def create_vector(self, lang, dataframe):  # , expandable):
         # there probably is a much better way to do this...
         vector = []
         for col_name, col_type in dataframe.dtypes.items():
-            new_columns = splittable.get(col_name)
+            new_columns = self.expanded_columns.get(col_name)
             if len(new_columns) > 0:  #  "if new_columns" does not work
                 counts = dataframe[col_name].value_counts(normalize=True)
                 for new_column in new_columns:
@@ -46,11 +53,14 @@ class DataAnalyser:
         return pd.Series(vector, name=lang)
 
 
-    def handle_data(self, dataframe_collection, splittable_columns=SPLITTABLE_COLS):
-        col_names = self.mark_splittable_columns(self.dataframe, splittable_columns)
-        series = [self.create_vector(lang, df, col_names) for lang, df in dataframe_collection.items()]
+    def simplify_df(self):  #  , dataframe_collection):  # , splittable_columns=EXPANDABLE_COLS):
+        #  col_names = self.mark_expandable_cols(self.df_collection, splittable_columns)
+        series = [
+                    self.create_vector(lang, df, self.expanded_columns)
+                    for lang, df in self.df_collection.items()
+                 ]
         new_col_names = []
-        for col_name, subcats in col_names.items():
+        for col_name, subcats in self.expanded_columns.items():
             if len(subcats) == 0:
                 new_col_names.append(col_name)
                 continue
@@ -61,6 +71,40 @@ class DataAnalyser:
         return vector_df
 
 
-    def draw_dendrogram(self):
-        dendrogram = sch.dendrogram(sch.linkage(self.data, method='ward'))
+    def draw_dendrogram(self, method='ward'):
+        sch.dendrogram(
+                        sch.linkage(self.data, method=method),
+                        labels=self.data.index,
+                        leaf_rotation=90
+                )
 
+
+    def show_clustering(self, algorithm, n_clusters):
+        clusters = algorithm(n_clusters=n_clusters).fit(self.data)
+        labels = clusters.labels_
+        #  self.show_clusters(labels)
+        print(f"Clustering with {algorithm.__name__}:\n")
+        for cluster_n in range(max(labels) + 1):
+            print(f"Cluster {cluster_n}:")
+            for group, lang in zip(labels, self.data.index):
+                if group == cluster_n:
+                    print(f"  {lang}")
+            print()
+        print("\n\n")
+
+
+    def draw_pca(self):
+        print('Dimensionality reduction via PCA decomposition:\n')
+        X_pca = PCA(2).fit_transform(self.data)
+        x = X_pca[:,0]
+        y = X_pca[:,1]
+
+        fig, ax = plt.subplots(figsize=(20, 14))
+        ax.scatter(x, y)
+        for i, label in enumerate(self.data.index):
+             ax.annotate(label, (x[i], y[i]))
+
+
+    def show_all_clusterings(self, n_clusters=5):
+        for clustering in (KMeans, AgglomerativeClustering):
+            self.show_clustering(clustering, n_clusters)
